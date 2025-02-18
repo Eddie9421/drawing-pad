b63e2a6efb4cc0cb6646e1fb49bb30f8961b8966
Eddie <Eddie11910@outlook.com>
2025-02-18 03:56:02 -0800

    Grid changing button is functional

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-window.addEventListener("load", () => {
+const MAXIMUM_NUMBER_OF_SQUARES = 100;
+
+function createGrid(squares) {
     const gridContainer = document.querySelector(".container");
+    gridContainer.replaceChildren();
 
     const box = document.createElement("div");
     box.classList.add("box");
@@ -7,20 +10,36 @@ window.addEventListener("load", () => {
     row.classList.add("row");
 
     // Set number of columns
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < squares; i++) {
         row.appendChild(box.cloneNode());
     }
 
     // Set number of rows
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < squares; i++) {
         gridContainer.appendChild(row.cloneNode(true));
     }
+    
+
 
     gridContainer.addEventListener("mouseover", (event) => {
     if (event.target.classList.contains("box")) {
         event.target.style.backgroundColor = "red";
     }
     });
+}
 
+window.addEventListener("load", () => {
+    
+    const gridChangerButton = document.querySelector(".grid-changer-button");
+
+    gridChangerButton.addEventListener("click", () => {
+        let squares = parseInt(prompt("Enter # of squares on each side:"));
+        if (squares > MAXIMUM_NUMBER_OF_SQUARES) {
+            alert(`You cannot create more than ${MAXIMUM_NUMBER_OF_SQUARES} squares.`)
+        }
+        else {
+            createGrid(squares);
+        }
+    })
 
 });
